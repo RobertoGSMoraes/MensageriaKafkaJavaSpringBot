5bdb36ff9e75a8c8ec1520655ee89c29b7fd9f02
sou-r <sou-r@DESKTOP-LE7KFBL>
2021-05-02 08:11:42 -0300

    Commit inicial.

---
+++ MensageriaKafkaJavaSpringBot/src/main/java/com/rm/MensageriaKafkaJavaSpringBotApplication.java
@@ -0,0 +1,13 @@
+package com.rm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensageriaKafkaJavaSpringBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensageriaKafkaJavaSpringBotApplication.class, args);
+	}
+
+}
---
+++ MensageriaKafkaJavaSpringBot/src/test/java/com/rm/MensageriaKafkaJavaSpringBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensageriaKafkaJavaSpringBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
